9fa8349a4224f4d6dd132f070a6a4f36bd126752
Paulo Sérgio Jr <paulo.psw@gmail.com>
2017-01-02 15:35:06 -0200

    Stub for stripes wallpaper

---
+++ stripes.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def stripes():
+    pass
+
+if __name__ == '__main__':
+    print('<svg xmlns="http://www.w3/org/2000/svg" width="1366" height="768">')
+    print(stripes())
+    print('</svg>')
