a5345b040faeb3c56ca997f481ceeb239e9fe994
Paulo Sergio Jr <paulo.psw@gmail.com>
2017-01-07 17:27:48 -0200

    Optionally narrow random color generation
    Function rand_color has now optional parameters. Given an rgb hex string, it will narrow the limits for random color generation. Useful to get less contrasting combinations.

--- stripes.py
+++ stripes.py
@@ -5,24 +5,29 @@ import random
 WIDTH = 1366
 HEIGHT = 768
 
-def rand_color():
-    r = random.choice(range(0,255))
-    g = random.choice(range(0,255))
-    b = random.choice(range(0,255))
+def rand_color(base=None, boundaries=50):
+    if base:
+        red, green, blue = int(base[1:3], 16), int(base[3:5], 16), int(base[5:], 16)
+        r = random.choice(range(red-boundaries, red+boundaries))
+        g = random.choice(range(green-boundaries, green+boundaries))
+        b = random.choice(range(blue-boundaries, blue+boundaries))
+    else:
+        r = random.choice(range(0,255))
+        g = random.choice(range(0,255))
+        b = random.choice(range(0,255))
     return '#{:02x}{:02x}{:02x}'.format(r,g,b)
-
 
 def stripes():
     columns = []
-    col_width = WIDTH/10
     while sum(columns) <= WIDTH:
+        col_width = random.choice( range(int(WIDTH/10), int(WIDTH/2)) )
         columns.append( col_width )
-
     x = 0
+    base_color = rand_color()
     for column in columns:
         print('<rect x="{x}" y="0" width="{col_width}" \
 height="{col_height}" style="fill:{color};stroke:none"\
-/>'.format(x=x, col_width=column, col_height=HEIGHT, color=rand_color()))
+/>'.format(x=x, col_width=column, col_height=HEIGHT, color=rand_color(base_color)))
         x += column
 
 if __name__ == '__main__':
