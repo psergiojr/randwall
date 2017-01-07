3ac5c70e63999cbabd9e72e7ddd632ba7ccb5c21
Paulo Sergio Jr <paulo.psw@gmail.com>
2017-01-07 16:20:49 -0200

    SVG syntax fixes

--- stripes.py
+++ stripes.py
@@ -15,18 +15,17 @@ def rand_color():
 def stripes():
     columns = []
     col_width = WIDTH/10
-    while sum(columns) < WIDTH:
+    while sum(columns) <= WIDTH:
         columns.append( col_width )
 
     x = 0
     for column in columns:
-        print('<rect x="{x}" y="0" width="{col_width}"\
-                height="{col_height}" style="fill:{color}"\
-                />'.format(x=x, col_width=column,
-                    col_height=HEIGHT, color=rand_color()))
+        print('<rect x="{x}" y="0" width="{col_width}" \
+height="{col_height}" style="fill:{color};stroke:none"\
+/>'.format(x=x, col_width=column, col_height=HEIGHT, color=rand_color()))
         x += column
 
 if __name__ == '__main__':
-    print('<svg xmlns="http://www.w3/org/2000/svg" width="1366" height="768">')
+    print('<svg xmlns="http://www.w3.org/2000/svg" width="1366" height="768">')
     stripes()
     print('</svg>')
